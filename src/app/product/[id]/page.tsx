96b6fc29eca11fd8598ab38b9f1fe92439851b67
Saida YENGUI <saida.yengui@gmail.com>
2025-08-02 12:36:24 +0100

    use next/image for optimized product image rendering in product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,14 +1,14 @@
-// app/product/[id]/page.tsx
-import { use } from 'react';
 import { products } from "@/data/products";
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 
 type ProductPageProps = {
   params: {
     id: string;
   };
 };
-export default  function ProductPage({ params }: { params: { id: string } }) {
+
+export default async function ProductPage({ params }: ProductPageProps) {
   const product = products.find((p) => p.id.toString() === params.id);
 
   if (!product) {
@@ -18,10 +18,20 @@ export default  function ProductPage({ params }: { params: { id: string } }) {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">{product.title}</h1>
-      <img src={product.image} alt={product.title} className="w-64 h-64 object-cover mb-4" />
+      <Image
+                src={product.image}
+                alt={product.title}
+                fill
+                className="object-cover rounded"
+                sizes="256px"
+      />
       <p className="text-lg mb-2">{product.description}</p>
-      <p className="text-xl font-semibold text-green-600">{product.price} USD</p>
-      <Link href="/" className="text-blue-600 underline mt-4 block">← Back to products</Link>
+      <p className="text-xl font-semibold text-green-600">
+        {product.price} USD
+      </p>
+      <Link href="/" className="text-blue-600 underline mt-4 block">
+        ← Back to products
+      </Link>
     </div>
   );
 }
